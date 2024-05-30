367daf2274fe16156c17b3916dff9ad7df834627
Gabriel Berserk <143458446+Gabsjormungand@users.noreply.github.com>
2024-05-29 21:21:00 -0300

    Adicionar arquivos de projeto.

---
+++ Banco de Dados/Form1.cs
@@ -0,0 +1,68 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Banco_de_Dados
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MySqlConnection conn = new MySqlConnection("server=localhost;user=root;database=bd_pokemon;password=;");
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM tb_pokemon", conn);
+
+            // DataTable = armazenar os resultados da consulta
+            DataTable dt = new DataTable();
+            conn.Open();
+
+            // MySqlDataAdapter = preencher o DataTable com os resultados da consulta
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            da.Fill(dt);
+            conn.Close();
+
+            // Vincule o DataTable ao DataGridView para exibir os dados na grade
+            dataGridView1.DataSource = dt;
+        }
+
+        private void txtpesquisa_TextChanged(object sender, EventArgs e)
+        {
+            string pesquisa = txtpesquisa.Text;
+            MySqlConnection conn = new MySqlConnection("server=localhost;user=root;database=bd_pokemon;password=;");
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM tb_pokemon where nome like '%"+pesquisa+"%'", conn);
+
+            // DataTable = armazenar os resultados da consulta
+            DataTable dt = new DataTable();
+            conn.Open();
+
+            // MySqlDataAdapter = preenchar o DataTable com os resultados da consulta
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            da.Fill(dt);
+            conn.Close();
+
+            // Vincule o DataTable ao DataGridView para exibir os dados na grade
+            dataGridView1.DataSource= dt;
+        }
+    }
+}
